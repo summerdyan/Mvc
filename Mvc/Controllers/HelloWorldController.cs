4f0d0d33210649988155f32ca59ed9a5d959b8db
summercrawford <sdc1579@gmail.com>
2022-03-09 15:12:56 -0600

    Added HelloWorldController based on Tutorial 1 Part 2.

---
+++ Mvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿// controller class that handles browser requests, retrieves model data, and calls
+// view templates that return a response
+// this controller class specifies a default action and a welcome action
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        // this is an HTTP GET method that's invoked by appending /HelloWorld/ to the base URL
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        // this is an HTTP GET method that's invoked by appending /HelloWorld/Welcome/ to the URL
+        // displays name and ID
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+        }
+    }
+}
