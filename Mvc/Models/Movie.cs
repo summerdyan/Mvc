e761125eb938d014d56669830948e2ec1a5c9403
summercrawford <sdc1579@gmail.com>
2022-03-09 21:33:57 -0600

    Practiced with controller actions and views. Fixed an issue with the Data namespace.

--- Mvc/Models/Movie.cs
+++ Mvc/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mvc.Models
 {
@@ -8,9 +9,15 @@ namespace Mvc.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        // shows "Release Date" on web application
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
+        // correctly maps Price to currency in the database
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
