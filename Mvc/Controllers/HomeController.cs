94a0a245b6cc4e2f97567408f9856f1923fc9322
summercrawford <sdc1579@gmail.com>
2022-03-11 12:29:12 -0600

    Added an About page.

--- Mvc/Controllers/HomeController.cs
+++ Mvc/Controllers/HomeController.cs
@@ -6,16 +6,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Mvc.Models;
+using Microsoft.EntityFrameworkCore;
+using Mvc.Data;
+using Mvc.Models.SchoolViewModels;
 
 namespace Mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MvcMovieContext _context; // variable for database context
 
-        public HomeController(ILogger<HomeController> logger)
+        // get an instance of context from the ASP.NET Core DI
+        public HomeController(ILogger<HomeController> logger, MvcMovieContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        // The LINQ statement groups the student entities by enrollment date,
+        // calculates the number of entities in each group,
+        // and stores the results in a collection of EnrollmentDateGroup view model objects
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            // display to view
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Index()
