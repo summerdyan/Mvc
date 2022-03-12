529a5797abfbbe7cad668592aa54ad21011bfc6c
summercrawford <sdc1579@gmail.com>
2022-03-11 20:59:41 -0600

    Created complex data model. Added migration. Updated databases.

---
+++ Mvc/Models/Department.cs
@@ -0,0 +1,30 @@
+﻿// create Department object
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mvc.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }   // ID associated with department
+
+        [StringLength(50, MinimumLength = 3)]   // name between 3 and 50 characters
+        public string Name { get; set; }        // name of department
+
+        [DataType(DataType.Currency)]           // data type associated with currency
+        [Column(TypeName = "money")]            // column will be defined using the SQL Server money type in the database
+        public decimal Budget { get; set; }     // budget of the department
+
+        [DataType(DataType.Date)]               // data type associated with date
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] // format the date
+        [Display(Name = "Start Date")]          // caption of text bos will be Start Date
+        public DateTime StartDate { get; set; } // Start Date of department
+
+        public int? InstructorID { get; set; }  // administrator's ID (? means it can be null)
+        public Instructor Administrator { get; set; }       // administrator
+
+        public ICollection<Course> Courses { get; set; }    // collection of courses offered by department
+    }
+}
--- Mvc/Models/Enrollment.cs
+++ Mvc/Models/Enrollment.cs
@@ -1,4 +1,7 @@
 ﻿// creating Enrollment object with associated course, student, and grade
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Mvc.Models
 {
     // establish the grade scale constants
@@ -12,6 +15,7 @@ namespace Mvc.Models
         public int EnrollmentID { get; set; }   // enrollment ID
         public int CourseID { get; set; }       // course ID
         public int StudentID { get; set; }      // enrolled student's ID
+        [DisplayFormat(NullDisplayText = "No grade")]   // if there's no grade, No grade is displayed
         public Grade? Grade { get; set; }       // grade (? means it can be null if grade is currently unknown or not assigned)
 
         public Course Course { get; set; }      // Course object associated with Enrollment object
