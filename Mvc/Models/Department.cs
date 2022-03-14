5ea8c008e46d149e7868875e7fdbc7b10fda3c2c
summercrawford <sdc1579@gmail.com>
2022-03-14 16:00:15 -0500

    Added Departments page and handled concurrency conflicts.

--- Mvc/Models/Department.cs
+++ Mvc/Models/Department.cs
@@ -13,16 +13,19 @@ namespace Mvc.Models
         [StringLength(50, MinimumLength = 3)]   // name between 3 and 50 characters
         public string Name { get; set; }        // name of department
 
-        [DataType(DataType.Currency)]           // data type associated with currency
-        [Column(TypeName = "money")]            // column will be defined using the SQL Server money type in the database
+        //[DataType(DataType.Currency)]           // data type associated with currency
+        //[Column(TypeName = "money")]            // column will be defined using the SQL Server money type in the database
         public decimal Budget { get; set; }     // budget of the department
 
         [DataType(DataType.Date)]               // data type associated with date
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] // format the date
         [Display(Name = "Start Date")]          // caption of text bos will be Start Date
         public DateTime StartDate { get; set; } // Start Date of department
-
+        
         public int? InstructorID { get; set; }  // administrator's ID (? means it can be null)
+
+        public Guid ConcurrencyToken { get; set; } = Guid.NewGuid();  // tracking property
+
         public Instructor Administrator { get; set; }       // administrator
 
         public ICollection<Course> Courses { get; set; }    // collection of courses offered by department
