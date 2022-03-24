d90f2974c61bbcbbf93b87821e3b952391f5f390
summerdyan <59991355+summerdyan@users.noreply.github.com>
2022-03-24 16:04:36 -0500

    Add files via upload

---
+++ Mvc/Data/MvcMovieContext.cs
@@ -0,0 +1,52 @@
+// creates a DbSet property for each entity set
+// An entity set typically corresponds to a database table.
+// An entity corresponds to a row in the table.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Mvc.Models;
+
+namespace Mvc.Data
+{
+    public class MvcMovieContext : IdentityDbContext<IdentityUser>
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+        }
+
+        // create Dbsets for all the following classes
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+
+        // changes table names to be singular
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Department>()
+                .Property(d => d.ConcurrencyToken)
+                .IsConcurrencyToken();
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            // configures the CourseAssignment entity's composite primary key
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
