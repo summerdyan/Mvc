79fceba1d658f4f705a43aa95e8c9ef0d7521a9d
summerdyan <59991355+summerdyan@users.noreply.github.com>
2022-03-24 19:47:18 -0500

    Add files via upload

---
+++ Mvc/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,15 @@
+﻿// keeps track of how many students have enrolled at a specific enrollment date
+// for About page of the web page
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)] // have data be in the form of a Date
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; } // number of students
+    }
+}
